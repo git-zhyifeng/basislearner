88a98c9775f54bcfdab4110081102984af1f7c85
git-zhyfieng <zhyifeng1989@gmail.com>
2013-12-20 09:49:35 +0800

    add function orth(), and complete the orthonormalize_approx.cpp

--- Cpp code/orthonormalize_approx.cpp
+++ Cpp code/orthonormalize_approx.cpp
@@ -5,6 +5,25 @@
 using namespace std;
 using namespace arma;
 
+
+mat orth(mat A)
+{
+	mat U;
+	vec s;
+	mat V;
+	svd(U, s, V, A);
+	double tol  = std::max(A.n_rows, A.n_cols) * s(0) * datum::eps;
+	int rank = ((uvec)find(s > tol)).n_elem;
+	mat retval;
+	if (rank > 0){
+		retval = -U.cols(0, rank - 1);
+	}
+	else{
+	    retval = zeros<mat>(A.n_rows, 0);
+	}
+	return retval;
+}
+
 //function orthonormalizes G and return weight matrix W such that
 //G*W is orthonormal, corresponding to largest 'width' singular values
 //uses approximate SVD: G*W is still orthonormal, but only approximately
@@ -21,7 +40,7 @@ mat orthonormalize_approx(mat G, int width)
 	mat L;
 	vec D;
 	mat W;
-	//svd_econ(L, D, W, orth(Y).t() * G);
+	svd_econ(L, D, W, orth(Y).t() * G);
 	W = W.cols(find(D > tol));
 	W = W.cols(0, std::min(width, (int)W.n_cols) - 1);
 
@@ -34,9 +53,14 @@ mat orthonormalize_approx(mat G, int width)
 	W = W * W2;
 	mat norms = sqrt(sum(pow(B,2), 0));
 	W = W / repmat(norms, W.n_rows, 1);
+	return W;
 }
 
 int main()
 {
+	mat A = ones<mat>(10,10);
+	for (int i = 1; i <= 10; i++)
+		A(0,i-1) = i;
+	cout  << orthonormalize_approx(A, 10) << endl;
 	return 0;
 }
